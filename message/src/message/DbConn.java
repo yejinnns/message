900d8016d897e51f89d460fac279b1b8b56c377e
yyeJinnn <yykk1244@naver.com>
2021-12-04 19:08:34 +0900

    데이터베이스 연동

--- message/src/message/DbConn.java
+++ message/src/message/DbConn.java
@@ -1,5 +1,39 @@
 package message;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DbConn {
+	Connection conn = null;
+
+	public DbConn() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("드라이버를 찾았습니다.");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3316/message", "root", "root");
+            System.out.println("데이터베이스에 성공적으로 연결 하였습니다.");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버를 검색 할 수 없습니다.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("데이터베이스 연결 실패!");
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		DbConn connection = new DbConn();
+		
+		
+		System.out.println("프로그램을 종료합니다.");
+	}
 
 }
+
+
+
+	
+
+
